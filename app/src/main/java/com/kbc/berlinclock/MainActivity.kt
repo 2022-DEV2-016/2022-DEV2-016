a5e1f7cc3306b6b89f53429ee21e49fbe12a926e
dev dev <dev.dev.055.dev@gmail.com>
2022-12-13 17:06:46 +0100

    feat(): initialize recycler view + add a layout for default item

--- app/src/main/java/com/kbc/berlinclock/MainActivity.kt
+++ app/src/main/java/com/kbc/berlinclock/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kbc.berlinclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kbc.berlinclock.adapters.BerlinClockAdapter
 import com.kbc.berlinclock.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var berlinClockAdapter: BerlinClockAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +20,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun initializeRecyclerView() {
         with(binding.activityMainRecyclerView) {
-
+            berlinClockAdapter = BerlinClockAdapter()
+            layoutManager = LinearLayoutManager(this.context)
+            adapter = berlinClockAdapter
+            visibility = View.VISIBLE
         }
     }
 }
--- app/src/main/java/com/kbc/berlinclock/interfaces/IDelegateAdapter.kt
+++ app/src/main/java/com/kbc/berlinclock/interfaces/IDelegateAdapter.kt
@@ -5,5 +5,5 @@ import androidx.recyclerview.widget.RecyclerView
 
 interface IDelegateAdapter {
     fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
-    fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int, viewType: IViewType)
+    fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int, viewType: ViewType)
 }
