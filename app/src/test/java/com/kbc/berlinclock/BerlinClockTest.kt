e22d3f38841abd756e5a0239f68f7dd4c971ff4b
dev dev <dev.dev.055.dev@gmail.com>
2022-12-13 15:49:37 +0100

    refactor(): test other cases for Berlin clock

--- app/src/test/java/com/kbc/berlinclock/BerlinClockTest.kt
+++ app/src/test/java/com/kbc/berlinclock/BerlinClockTest.kt
@@ -49,4 +49,71 @@ class BerlinClockTest {
         Assert.assertEquals(7, blocks.minuteFirstRow)
         Assert.assertEquals(2, blocks.minuteSecondRow)
     }
+
+    @Test
+    fun `should returns the class Blocks and isOdd when gives the hour 23 and minutes 59`() {
+        val blocks = giveMeBlocksForHour(hour = 23, minute = 59)
+        val isOdd = isOdd(number = 59)
+        Assert.assertTrue(isOdd)
+        Assert.assertEquals(4, blocks.hourFirstRow)
+        Assert.assertEquals(3, blocks.hourSecondRow)
+        Assert.assertEquals(11, blocks.minuteFirstRow)
+        Assert.assertEquals(4, blocks.minuteSecondRow)
+    }
+
+    @Test
+    fun `should returns the class Blocks and isOdd when gives the hour 0 and minutes 0`() {
+        val blocks = giveMeBlocksForHour(hour = 0, minute = 0)
+        val isOdd = isOdd(number = 0)
+        Assert.assertFalse(isOdd)
+        Assert.assertEquals(0, blocks.hourFirstRow)
+        Assert.assertEquals(0, blocks.hourSecondRow)
+        Assert.assertEquals(0, blocks.minuteFirstRow)
+        Assert.assertEquals(0, blocks.minuteSecondRow)
+    }
+
+    @Test
+    fun `should returns the class Blocks and isOdd when gives the hour 3 and minutes 9`() {
+        val blocks = giveMeBlocksForHour(hour = 3, minute = 9)
+        val isOdd = isOdd(number = 51)
+        Assert.assertTrue(isOdd)
+        Assert.assertEquals(0, blocks.hourFirstRow)
+        Assert.assertEquals(3, blocks.hourSecondRow)
+        Assert.assertEquals(1, blocks.minuteFirstRow)
+        Assert.assertEquals(4, blocks.minuteSecondRow)
+    }
+
+
+    @Test
+    fun `should returns the class Blocks and isOdd when gives the hour 12 and minutes 2`() {
+        val blocks = giveMeBlocksForHour(hour = 12, minute = 2)
+        val isOdd = isOdd(number = 47)
+        Assert.assertTrue(isOdd)
+        Assert.assertEquals(2, blocks.hourFirstRow)
+        Assert.assertEquals(2, blocks.hourSecondRow)
+        Assert.assertEquals(0, blocks.minuteFirstRow)
+        Assert.assertEquals(2, blocks.minuteSecondRow)
+    }
+
+    @Test
+    fun `should returns the class Blocks and isOdd when gives the hour 22 and minutes 55`() {
+        val blocks = giveMeBlocksForHour(hour = 22, minute = 55)
+        val isOdd = isOdd(number = 50)
+        Assert.assertFalse(isOdd)
+        Assert.assertEquals(4, blocks.hourFirstRow)
+        Assert.assertEquals(2, blocks.hourSecondRow)
+        Assert.assertEquals(11, blocks.minuteFirstRow)
+        Assert.assertEquals(0, blocks.minuteSecondRow)
+    }
+
+    @Test
+    fun `should returns the class Blocks and isOdd when gives the hour 15 and minutes 55`() {
+        val blocks = giveMeBlocksForHour(hour = 15, minute = 0)
+        val isOdd = isOdd(number = 0)
+        Assert.assertFalse(isOdd)
+        Assert.assertEquals(3, blocks.hourFirstRow)
+        Assert.assertEquals(0, blocks.hourSecondRow)
+        Assert.assertEquals(0, blocks.minuteFirstRow)
+        Assert.assertEquals(0, blocks.minuteSecondRow)
+    }
 }
