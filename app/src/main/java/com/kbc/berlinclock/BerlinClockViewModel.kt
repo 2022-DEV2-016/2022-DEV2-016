468f63e1be8f9a1308aa0aaadecb0f9ae10da636
dev dev <dev.dev.055.dev@gmail.com>
2022-12-13 16:52:45 +0100

    feat(): add drawables

--- app/src/main/java/com/kbc/berlinclock/BerlinClockViewModel.kt
+++ app/src/main/java/com/kbc/berlinclock/BerlinClockViewModel.kt
@@ -24,7 +24,6 @@ class BerlinClockViewModel: ViewModel() {
 
     fun starts() {
         viewModelScope.launch(Dispatchers.IO) {
-
             everySeconds()
                 .collect {
                     with(Calendar.getInstance()) {
