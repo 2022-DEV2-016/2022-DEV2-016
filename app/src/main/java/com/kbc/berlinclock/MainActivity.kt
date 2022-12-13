83aeb48028459c48f42b1ff2805773678912d0ba
dev dev <dev.dev.055.dev@gmail.com>
2022-12-13 16:59:01 +0100

    refactor(): show recycler view for lamp and blocks + text for hour

--- app/src/main/java/com/kbc/berlinclock/MainActivity.kt
+++ app/src/main/java/com/kbc/berlinclock/MainActivity.kt
@@ -2,10 +2,21 @@ package com.kbc.berlinclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kbc.berlinclock.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initializeRecyclerView()
+    }
+
+    private fun initializeRecyclerView() {
+        with(binding.activityMainRecyclerView) {
+
+        }
     }
 }
