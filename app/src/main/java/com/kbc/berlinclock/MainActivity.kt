547d84c21837f9d66c9908db24109e3bc07a9d7f
dev dev <dev.dev.055.dev@gmail.com>
2022-12-13 17:12:10 +0100

    feat(): get value from blocks live data

--- app/src/main/java/com/kbc/berlinclock/MainActivity.kt
+++ app/src/main/java/com/kbc/berlinclock/MainActivity.kt
@@ -2,7 +2,9 @@ package com.kbc.berlinclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kbc.berlinclock.adapters.BerlinClockAdapter
 import com.kbc.berlinclock.databinding.ActivityMainBinding
@@ -10,12 +12,21 @@ import com.kbc.berlinclock.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var berlinClockAdapter: BerlinClockAdapter
+    val berlinClockViewModel: BerlinClockViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initializeRecyclerView()
+        with(berlinClockViewModel) {
+            starts()
+            berlinClockLiveData.observe(this@MainActivity, this@MainActivity::manageBlocks)
+        }
+    }
+
+    private fun manageBlocks(blocks: Blocks) {
+        Log.d("testbnp","receive result here $blocks --")
     }
 
     private fun initializeRecyclerView() {
